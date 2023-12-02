c6589878ee532da96ae767c5f12889403279b4c4
Aaquib Equbal <armanaaquibequbal@gmail.com>
2023-12-02 11:14:38 +0530

    Add day01 & day02 solution

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,1 +1,54 @@
 package day01
+
+import println
+import readInputAsLines
+
+fun main() {
+    fun findCalibrationValue(text: String): Int {
+        val fistDigit = text.first { it.isDigit() }
+        val lastDigit = text.last { it.isDigit() }
+        return (fistDigit.toString() + lastDigit).toInt()
+    }
+
+    fun part1(input: List<String>): Int {
+        return input.fold(0) { sum, text ->
+            sum + findCalibrationValue(text)
+        }
+    }
+    check(part1(readInputAsLines("day01p1_test")) == 142)
+    part1(readInputAsLines("day01")).println()
+
+    /*Do not work for "twone"
+    fun findCalibrationValue(text: String): Int {
+        val regex = "(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9)".toRegex()
+        val digits = mapOf(
+            "one" to "1", "two" to "2", "three" to "3", "four" to "4", "five" to "5",
+            "six" to "6", "seven" to "7", "eight" to "8", "nine" to "9"
+        )
+        val matches = regex.findAll(text)
+        val firstDigit = matches.first().value
+        val secondDigit = matches.last().value
+        return ((digits[firstDigit] ?: firstDigit) + (digits[secondDigit] ?: secondDigit)).toInt()
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.fold(0) { sum, text -> sum + findCalibrationValue(text)
+        }
+    }
+     */
+
+    fun part2(input: List<String>): Int {
+        return input.fold(0) { sum, text ->
+            var newText = text
+            listOf(
+                "one" to "o1e", "two" to "t2o", "three" to "t3e", "four" to "f4r", "five" to "f5e",
+                "six" to "s6x", "seven" to "s7n", "eight" to "e8t", "nine" to "n9e"
+            ).forEach {
+                newText = newText.replace(it.first, it.second)
+            }
+            sum + findCalibrationValue(newText)
+        }
+    }
+    check(part2(readInputAsLines("day01p2_test")) == 281)
+    part2(readInputAsLines("day01")).println()
+}
---
+++ src/day02/Day02.kt
@@ -0,0 +1,61 @@
+package day02
+
+import println
+import readInputAsLines
+
+data class Cube(val color: String, val count: Int)
+data class Game(val id: Int, val cubes: List<Cube>)
+
+fun main() {
+
+    fun parseCubes(line: String): List<Cube> {
+        return line.split(": ")[1].split("(, |; )".toRegex()).map {
+            val countAndColour = it.split(" ")
+            Cube(countAndColour[1], countAndColour[0].toInt())
+        }
+    }
+
+    fun parseGames(input: List<String>): List<Game> {
+        return input.mapIndexed { idx, line ->
+            Game(idx + 1, parseCubes(line))
+        }
+    }
+
+    fun isGamePossible(game: Game): Boolean {
+        val bag = mapOf("red" to 12, "green" to 13, "blue" to 14)
+        for (cube in game.cubes) {
+            if (cube.count > bag[cube.color]!!) {
+                return false
+            }
+        }
+        return true
+    }
+
+    fun part1(input: List<String>): Int {
+        val games = parseGames(input)
+        return games.fold(0) { sum, game ->
+            if (isGamePossible(game))
+                sum + game.id
+            else
+                sum
+        }
+    }
+    check(part1(readInputAsLines("day02_test")) == 8)
+    part1(readInputAsLines("day02")).println()
+
+    fun powerOfMinimumSetOfCubes(game: Game): Int {
+        val groupedCubesByColor = game.cubes.groupBy { it.color }
+        return groupedCubesByColor.values.fold(1) { power, cube ->
+            power * cube.maxBy { it.count }.count
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        val games = parseGames(input)
+        return games.fold(0) { sum, game ->
+            sum + powerOfMinimumSetOfCubes(game)
+        }
+    }
+    check(part2(readInputAsLines("day02_test")) == 2286)
+    part2(readInputAsLines("day02")).println()
+}
